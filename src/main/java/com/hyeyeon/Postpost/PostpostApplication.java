7057b832a59d87f7a3708922c1f785d770442415
Hyeyeon-Ko <khy33355@gmail.com>
2024-02-13 13:41:47 +0900

    Initial Commit

---
+++ src/main/java/com/hyeyeon/Postpost/PostpostApplication.java
@@ -0,0 +1,13 @@
+package com.hyeyeon.Postpost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostpostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostpostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hyeyeon/Postpost/PostpostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyeyeon.Postpost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostpostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
