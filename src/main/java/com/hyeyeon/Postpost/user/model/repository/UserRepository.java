246d8db77f5b769543ce96138d19980e267402cb
Hyeyeon-Ko <khy33355@gmail.com>
2024-02-17 10:53:45 +0900

    fix: 중복닉네임 코드 주석처리

--- src/main/java/com/hyeyeon/Postpost/user/model/repository/UserRepository.java
+++ src/main/java/com/hyeyeon/Postpost/user/model/repository/UserRepository.java
@@ -3,10 +3,14 @@ package com.hyeyeon.Postpost.user.model.repository;
 import com.hyeyeon.Postpost.user.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByName(String name);
 
-    boolean isDuplicationNickname(String email, String nickname);
+//    boolean isDuplicationNickname(String email, String nickname);
 
+    List<User> findByBirthday(LocalDate birthday);
 }
