61a1808b6c0ec82c55bff7344fbd9756f70f7800
Hyeyeon-Ko <khy33355@gmail.com>
2024-02-21 22:15:48 +0900

    style: 필요없는 코드 삭제

--- src/main/java/com/hyeyeon/Postpost/user/controller/MainController.java
+++ src/main/java/com/hyeyeon/Postpost/user/controller/MainController.java
@@ -1,29 +1,27 @@
 package com.hyeyeon.Postpost.user.controller;
 
+import com.hyeyeon.Postpost.post.model.dto.IconRankDto;
 import com.hyeyeon.Postpost.post.model.dto.SharePostDto;
 import com.hyeyeon.Postpost.post.model.dto.UserPostDto;
 import com.hyeyeon.Postpost.post.model.service.PostService;
+import com.hyeyeon.Postpost.user.model.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class MainController {
 
+    private final UserService userService;
     private final PostService postService;
-
-    @GetMapping({"", "/"})
-    public String index() {
-        return "index";
-    }
 
     // 공유 일기창
     @GetMapping("/share")
@@ -42,8 +40,17 @@ public class MainController {
         List<UserPostDto> mailboxList = postService.getUserPost(userId, targetDate);
         result.put("mailboxList", mailboxList);
 
-        // 닉네임, 작성한 post수, 아이콘 Top3
+        // 닉네임
+        String nickname = userService.getNickname(userId);
+        result.put("userNickname", nickname);
 
+        // 작성한 포스트 수
+        int postCnt = mailboxList.size();
+        result.put("postCnt", postCnt);
+
+        //아이콘 Top3
+        List<IconRankDto> top3Icon = postService.getTop3Icon(userId);
+        result.put("top3Icon", top3Icon);
 
         return ResponseEntity.ok(result);
     }
